0af9922b175a24ffd152aa4c7d476aa70f9235a1
vyom <sharm843@umn.edu>
2022-08-09 20:13:08 -0400

    Single Project Query and Client info

---
+++ client/src/components/ClientInfo.jsx
@@ -0,0 +1,22 @@
+import { FaPhone, FaEnvelope, FaIdBadge } from "react-icons/fa";
+
+function ClientInfo(props) {
+    return (
+        <>
+            <h5 className="mt-5">Client Information</h5>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <FaIdBadge className="icon" /> {props.client.name}
+                </li>
+                <li className="list-group-item">
+                    <FaEnvelope className="icon" /> {props.client.email}
+                </li>
+                <li className="list-group-item">
+                    <FaPhone className="icon" /> {props.client.phone}
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default ClientInfo;
